2a387c7eedad7b1c612f6310e7f2545e5cba6a53
Yutaka <nomura.yutaka+gitkraken@gmail.com>
2019-08-18 16:04:30 +0900

    Add dropboxes to control axes, colors, etc.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.express as px
+from dash.dependencies import Input, Output
 
 colors = {
     'background': '#111111',
@@ -11,37 +12,55 @@ colors = {
 
 df = pd.read_csv('laser_data.csv')
 
-fig = px.scatter(df, x='lc', y='average_power', log_y=True, color='gain_medium')
-fig.update_traces(marker=dict(size=12),
-                  selector=dict(mode='markers'))
+col_options = [dict(label=x, value=x) for x in df.columns]
+dimensions = ['x', 'y', 'color', 'facet_col', 'facet_row']
 
 # Prepare the view
 app = dash.Dash()
 server = app.server
 
-app.layout = html.Div(children=[
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
+app.layout = html.Div(
+    [
+        html.H1(
+            children='Fiber Laser Parameters',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }
+        ),
 
-    html.Div(
-        children='Dash: A web application framework for Python',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
+        html.Div(
+            [
+                html.P([d + ":", dcc.Dropdown(id=d, options=col_options)])
+                for d in dimensions
+            ],
+            style={"width": "25%", "float": "left"},
+        ),
 
-    dcc.Graph(
-        id='laser-data',
-        figure=fig
-    ),
-])
+        dcc.Graph(
+            id='laser-data-graph',
+            style={"width": "75%", "display": "inline-block"}
+        ),
+    ]
+)
 
+@app.callback(
+    Output('laser-data-graph', 'figure'),
+    [Input(d, 'value') for d in dimensions]
+)
+def make_figure(x, y, color, facet_col, facet_row):
+    fig = px.scatter(
+        df,
+        x=x,
+        y=y,
+        color=color,
+        facet_col=facet_col,
+        facet_row=facet_row,
+        height=700,
+    )
+    fig.update_traces(marker=dict(size=12),
+                      selector=dict(mode='markers'))
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
